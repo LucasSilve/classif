c80df07e379083b66c2292e8644c103346cebdb3
LucasSilve <silve.lucas@yahoo.fr>
2018-07-23 14:59:39 +0200

    mobilenet from compressed data

--- main.py
+++ main.py
@@ -1,20 +1,88 @@
 '''Train CIFAR10 with PyTorch.'''
 from __future__ import print_function
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-
 import torchvision
 import torchvision.transforms as transforms
-
 import os
 import argparse
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import scipy
+import numpy as np
+from numpy.fft import fftshift
+from scipy.fftpack import fft2
+import matplotlib.pyplot as plt
+import torch.optim as optim
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from torch.autograd import Variable
 
-from models import *
+
 from utils import progress_bar
+
+channel_=3
+padding_=1
+kernel=2*padding_+1
+imsize=32
+num_classes=10
+
+
+class Block(nn.Module):
+    '''Depthwise conv + Pointwise conv'''
+    def __init__(self, in_planes, out_planes, stride=1):
+        super(Block, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False)
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        return out
+
+
+class MobileNet(nn.Module):
+    # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
+    #cfg = [64, (128,2), 128, (256,2), 256, (512,2), 512, 512, 512, 512, 512, (1024,2), 1024]
+    #cfg = [64, (128, 2), 128, (256, 2), 256, (512, 2), 512, 512, 512, (1024, 2), 1024]
+    cfg = [16, (32, 2), 32, (64, 2), 64, (128, 2), 128, 128, 128, 128, 128, (256, 2), 256]
+    def __init__(self, num_classes=10):
+        super(MobileNet, self).__init__()
+        self.conv1 = nn.Conv2d(channel_,8 , kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(8)
+        self.layers = self._make_layers(in_planes=8)
+        self.linear = nn.Linear(256, num_classes)
+
+    def _make_layers(self, in_planes):
+        layers = []
+        for x in self.cfg:
+            out_planes = x if isinstance(x, int) else x[0]
+            stride = 1 if isinstance(x, int) else x[1]
+            layers.append(Block(in_planes, out_planes, stride))
+            in_planes = out_planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layers(out)
+        out = F.avg_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def test():
+    net = MobileNet()
+    x = torch.randn(1,3,8,8)
+    y = net(x)
+    print('testing :')
+    print(y.size())
+
+test()
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
@@ -64,13 +132,9 @@ net = MobileNet()
 
 
 
-channel_=3
-padding_=1
-kernel=2*padding_+1
-imsize=32
-num_classes=10
 
-class Block(nn.Module):
+
+"""class Block(nn.Module):
     def __init__(self,channel):
 
         super(Block,self).__init__()
@@ -148,15 +212,136 @@ class Net(nn.Module):
         out=self.linear(out)
 
 
-        return out
+        return out"""
 
 
 
 #net=Net(Block,[2,2,2,5,10])
 
 
+batch=1
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch,
+                                         shuffle=False, num_workers=2)
+
+
+#initialisation
+
+lum_seul=True
+if lum_seul:
+    channel=1
+else: channel=3
+nombre_filtre=16
+padding_=5
+kernel=2*padding_+1
+learning_rate=0.05
+lambda_regular=1
+
+
+def pad(f):                                         #effectue un padding pour le filtre pour pouvoir faire la transformée de Fourier
+    out=np.zeros((32,32), dtype=complex)
+    out[15-padding_:16+padding_,15-padding_:16+padding_]=f
+    return out
+
+def colorbar(mappable):
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    return fig.colorbar(mappable, cax=cax)
+
+def gabor_2d(M, N, sigma, theta, xi, slant=1.0, offset=0, fft_shift=None):
+    gab = np.zeros((M, N), np.complex64)
+    R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]], np.float32)
+    R_inv = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]], np.float32)
+    D = np.array([[1, 0], [0, slant * slant]])
+    curv = np.dot(R, np.dot(D, R_inv)) / ( 2 * sigma * sigma)
+
+    for ex in [0]:
+        for ey in [0]:
+            [xx, yy] = np.mgrid[offset + ex * M:offset + M + ex * M, offset + ey * N:offset + N + ey * N]
+            arg = -(curv[0, 0] * np.multiply(xx, xx) + (curv[0, 1] + curv[1, 0]) * np.multiply(xx, yy) + curv[
+                1, 1] * np.multiply(yy, yy)) + 1.j * (xx * xi * np.cos(theta) + yy * xi * np.sin(theta))
+            gab = gab + np.exp(arg)
+
+    norm_factor = (2 * 3.1415 * sigma * sigma / slant)
+    gab = gab / norm_factor
+
+    if (fft_shift):
+        gab = np.fft.fftshift(gab, axes=(0, 1))
+    return gab
+
+phi=gabor_2d(kernel,kernel,3,0,0,fft_shift=False,offset=-padding_)
+phi=np.absolute(phi)
+phi_chap=pad(phi)
+phi_chap=fft2(phi_chap)
+phi_chap=fftshift(phi_chap)
+phi_chap=np.absolute(phi_chap)
+
+fig, (ax1, ax2) = plt.subplots(ncols=2)
+
+img1 = ax1.imshow(phi)
+colorbar(img1)
+
+img2 = ax2.imshow(phi_chap)
+colorbar(img2)
+plt.tight_layout(h_pad=1)
+plt.show()
+
+
+print('ok')
+phi=torch.from_numpy(phi)
+print(torch.sum(phi))
+phi=phi/torch.sum(phi)
+
+
+avg=nn.Conv2d(nombre_filtre,nombre_filtre,kernel,bias=False,padding=padding_,groups=nombre_filtre)
+avg=avg.cuda()
+
+for k1 in range(0,nombre_filtre):
+    avg.weight.data[k1,0,:,:]=phi
+
+
+class Net1(nn.Module):
+    def __init__(self, nombre_filtre):
+        super(Net1, self).__init__()
+        self.conv_real = nn.Conv2d(channel, nombre_filtre, kernel, bias=False,padding=padding_)
+        self.conv_imag = nn.Conv2d(channel, nombre_filtre, kernel, bias=False, padding=padding_)
+
+        self.avg = torch.nn.AvgPool2d(5,stride=1,padding=2,count_include_pad=True)
+        self.bn = nn.BatchNorm2d(nombre_filtre)
+    def forward(self, x):
+        y_r = self.conv_real(x)
+        y_i = self.conv_imag(x)
+
+        y_r = y_r ** 2
+        y_i = y_i ** 2
+        y = y_r + y_i
+        y = torch.sqrt(y)
+        y = avg(y)
+
+        return y
+
+
+Net1 = Net1(nombre_filtre).cuda()
+poids=torch.load('/home/lucas/Pycharm/pycharm-2018.1.4/bin/lucas_code_beginning/save/WN.pth.tar')
+Net1.load_state_dict(poids)
 
 net = net.to(device)
+
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
@@ -187,7 +372,15 @@ def train(epoch):
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimi.zero_grad()
-        outputs = net(inputs)
+        if lum_seul:
+            inputs = torch.sum(inputs, 1, keepdim=True)
+            aux = torch.ones(1)
+            aux = 3 * aux
+            aux = torch.sqrt(aux)
+            aux = aux.item()
+            inputs = inputs / aux
+        compressedinputs = Net1(inputs)
+        outputs = net(compressedinputs)
         loss = criterion(outputs, targets)
         loss.backward()
         optimi.step()
