42648b5e689d10e62ea8fe19379b8a43549938ab
LucasSilve <silve.lucas@yahoo.fr>
2018-07-24 10:17:46 +0200

    no batch norm

--- main.py
+++ main.py
@@ -71,7 +71,8 @@ class MobileNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        #out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.conv1(x))
         out = self.layers(out)
         out = F.avg_pool2d(out, 2)
         out = out.view(out.size(0), -1)
